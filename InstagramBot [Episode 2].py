d0b6b20ca571b40da30286a9be7a1e59a3d8c7aa
farzany <59945358+farzany@users.noreply.github.com>
2021-01-09 22:16:51 -0500

    Add files via upload

---
+++ InstagramBot [Episode 2].py
@@ -0,0 +1,249 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+import random
+import numpy
+
+class InstagramBot:
+
+	def __init__(self,username,password):
+		self.loggedin = False
+		self.username = username
+		self.password = password
+		self.base_url = "https://www.instagram.com"
+		self.followersCount = 0
+		self.followingCount = 0
+		self.followedList = []
+		self.doNotFollowList = []
+		self.whitelist = []
+		self.CreateFiles()
+		self.Choose()
+
+	################# Chooser #################
+
+	def Choose(self):
+		# The UI
+		if not self.loggedin:
+			self.Login()
+			self.GetInfo()
+		print(".................")
+		choice = input("[1] Follow Followers\n[2] Unfollow Followed\n[3] Unfollow All\nChoice:\t")
+		if choice == "1":
+			self.FollowFollowers()
+			self.Choose()
+		elif choice == "2":
+			self.UnfollowFollowed()
+			self.Choose()
+		elif choice == "3":
+			self.UnfollowAll()
+			self.Choose()
+		elif choice == "q":
+			return
+
+	################# Helper Methods #################
+
+	def ConvertToNumber(self, text):
+		# Converts instagram's shorthand to a number
+		if "k" in text:
+			return int((float(text.replace("k","")))*1000)
+		elif "m" in text:
+			return int((float(text.replace("m","")))*1000000)
+		else:
+			return int(text)
+	
+	def Wait(self,min,max):
+		# Pauses for a random amount of time
+		time.sleep(random.choice(numpy.arange(min,max,0.1)))
+
+	def GoToUser(self, user):
+		# Goes to a user's page
+		self.driver.get("{}/{}/".format(self.base_url, user))
+		self.Wait(1,2)
+
+	def UnfollowUser(self, user):
+		# Unfollows a user through their page
+		self.GoToUser(user)
+		div = self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/div[1]")
+		button = div.find_elements_by_xpath(".//button")
+		if (button and button[0].text != "Follow"):
+			if (len(button) > 2):
+				button[1].click()
+			else:
+				button[0].click()
+			self.Wait(1,1.5)
+			self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div/div[3]/button[1]").click()
+
+	def CreateFiles(self):
+		# Creates files for user if they don't exist
+		temp = open("[Followed][{}]".format(self.username), "a+")
+		temp.close
+		temp = open("[DoNotFollow][{}]".format(self.username), "a+")
+		temp.close
+		temp = open("[Whitelist][{}]".format(self.username), "a+")
+		temp.close
+
+	def ReadFiles(self):
+		# Obtains previously followed usernames from file
+		self.Wait(1,1.5)
+		self.followedList = []
+		self.doNotFollowList = []
+		self.whitelist = []
+		with open("[Followed][{}]".format(self.username), "r+") as flist:
+			lines = flist.readlines()
+			for line in lines:
+				self.followedList.append(line.strip())
+		with open("[DoNotFollow][{}]".format(self.username), "r+") as flist:
+			lines = flist.readlines()
+			for line in lines:
+				self.doNotFollowList.append(line.strip())
+		with open("[Whitelist][{}]".format(self.username), "r+") as flist:
+			lines = flist.readlines()
+			for line in lines:
+				self.whitelist.append(line.strip())
+
+	def AddFollowedList(self, name):
+		# Appends followed username to files
+		with open("[Followed][{}]".format(self.username), "a+") as flist:
+			temp = name.strip() + "\n"
+			flist.write(temp)
+		self.ReadFiles()
+
+	def AddDoNotFollowList(self, name):
+		# Adds a user to DoNotFollow file
+		with open("[DoNotFollow][{}]".format(self.username), "a+") as flist:
+			temp = name.strip() + "\n"
+			flist.write(temp)
+		self.ReadFiles()
+	
+	def RemFollowedList(self, name):
+		# Removes followed username from file
+		with open("[Followed][{}]".format(self.username), "r") as flist:
+			lines = flist.readlines()
+		with open("[Followed][{}]".format(self.username), "w") as flist:
+			for line in lines:
+				if line.strip() != name:
+					flist.write(line)
+		self.ReadFiles()
+
+
+	################# Logging In #################
+
+	def Login(self):
+		self.driver = webdriver.Chrome("chromedriver.exe")
+		print("Logging In: {}".format(self.username))
+		self.driver.get("{}/accounts/login".format(self.base_url))
+		self.Wait(2,3)
+		self.driver.find_element_by_name("username").send_keys(self.username)
+		self.Wait(1,2)
+		self.driver.find_element_by_name("password").send_keys(self.password)
+		self.Wait(1,2)
+		self.driver.find_elements_by_xpath("//div[contains(text(), 'Log In')]")[0].click()
+		print("Logged In: {}".format(self.username))
+		self.loggedin = True
+		self.Wait(5,6)
+
+	def GetInfo(self):
+		# Gets followers & following amounts
+		self.GoToUser(self.username)
+		# Get number of followers
+		temp = self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a/span").text
+		self.followersCount = self.ConvertToNumber(temp)
+		# Get number of following
+		temp = self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
+		self.followingCount = self.ConvertToNumber(temp)
+		# print(self.followersCount, self.followingCount)
+		self.ReadFiles()
+
+
+	################# Following Followers #################
+
+	def FollowFollowers(self):
+		# Follows the followers of a user
+		# Gets username and number of followers to follow
+		self.ReadFiles() # In case of any changes to files
+		user = input("Account username:\t")
+		self.GoToUser(user)
+		temp = self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a/span").text
+		numoffollowers = self.ConvertToNumber(temp)
+		amount = int(input("How many to follow? (Less than {})\t".format(temp)))
+		while amount > numoffollowers:
+			amount = int(input("How many to follow? (Less than {})\t".format(temp)))
+
+		# Clicks followers tab and goes through the list one by one
+		self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a").click()
+		i, k = 1, 1
+		while (k <= amount):
+			self.Wait(1,1.5)
+			currentUser = self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]".format(i))
+			button = currentUser.find_elements_by_xpath(".//button")
+			name = currentUser.find_element_by_css_selector(".notranslate").text
+			# If a strictly "Follow" button exists, it clicks it
+			if (button) and (button[0].text == "Follow") and (name not in self.doNotFollowList):
+				self.Wait(30,35)
+				button[0].click()
+				self.AddFollowedList(name) # Writes username to file
+				self.AddDoNotFollowList(name)
+				print("[{}] {} followed {}".format(k, self.username, name))
+				k += 1
+			self.Wait(1,1.5)
+			# Scrolls down for the user to be at the top of the tab
+			self.driver.execute_script("arguments[0].scrollIntoView()", currentUser)
+			i += 1
+
+	################# Unfollowing Followed #################
+
+	def UnfollowFollowed(self):
+		self.ReadFiles() # In case of any changes to files
+		numoffollowed = len(self.followedList)
+		amount = int(input("How many to unfollow? (followed {})\t".format(numoffollowed)))
+		while amount > numoffollowed:
+			amount = int(input("How many to unfollow? (followed {})\t".format(numoffollowed)))
+
+		for i in range(amount):
+			user = self.followedList[0]
+			self.Wait(10,15)
+			self.UnfollowUser(user)
+			self.RemFollowedList(user)
+			print("[{}] {} unfollowed {}".format(i+1, self.username, user))
+
+	################# Unfollowing All #################
+
+	def UnfollowAll(self):
+		# Unfollows a certain number of following (at random)
+		# Adds names to DoNotFollow if not already there
+		# Removes names from Followed if there
+		self.GetInfo()
+		amount = int(input("How many to unfollow? (following {})\t".format(self.followingCount)))
+		while amount > self.followingCount:
+			amount = int(input("How many to unfollow? (following {})\t".format(self.followingCount)))
+
+		self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a").click()
+		i, k = 1, 1
+		while (k <= amount):
+			self.Wait(1,1.5)
+			currentUser = self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]".format(i))
+			button = currentUser.find_elements_by_xpath(".//button")
+			name = currentUser.find_element_by_css_selector(".notranslate").text
+			# If a strictly "Following" button exists, click it
+			if (button) and (button[0].text == "Following") and (name not in self.whitelist):
+				self.Wait(30,35)
+				button[0].click()
+				if name in self.followedList:
+					self.RemFollowedList(name) # Removes username from file
+				if name not in self.doNotFollowList:
+					self.AddDoNotFollowList(name) # Adds username to file
+				self.Wait(1,2)
+				# Click unfollow in prompt
+				self.driver.find_element_by_xpath("/html/body/div[6]/div/div/div/div[3]/button[1]").click()
+				print("[{}] {} unfollowed {}".format(k, self.username, name))
+				k += 1
+			self.Wait(1,1.5)
+			# Scroll down for user to be at the top of the tab
+			self.driver.execute_script("arguments[0].scrollIntoView();", currentUser)
+			i += 1
+
+
+TestRuns = InstagramBot("your username", "your password")
+
+	
